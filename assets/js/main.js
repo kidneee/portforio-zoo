32ea0e1fc153f1d028baca009eef484e4424c6e5
kidoneee <k.shi8519@gmail.com>
2025-08-01 16:00:46 +0900

    Add responsive hamburger menu with HTML, CSS, and JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,10 @@ const drawerMenu = document.getElementById('js-drawerMenu');
 hamburgerBtn.addEventListener('click', () => {
   drawerMenu.classList.toggle('open');
   hamburgerBtn.classList.toggle('open');
+
+  if (drawerMenu.classList.contains('open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
 });
